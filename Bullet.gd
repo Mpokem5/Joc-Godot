a3622106abdc96b27aaede8c34f9e2bf3af28726
Marc Garcia Clusella <marccc.garcia@gmail.com>
2021-06-09 00:32:43 +0200

    Moviment i dispar

---
+++ Bullet.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var SPEED = 500
+var direction : Vector2
+var shooter : Node
+
+
+# Called when the node enters the scene tree for the first time.
+func ini(dir:Vector2, pos:Vector2) -> void:
+	position = pos
+	direction = dir
+	rotation = direction.angle()
+
+func _physics_process(delta):
+	position += direction * SPEED * delta
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Player.gd
@@ -0,0 +1,66 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var bullet = preload("res://Scenes/Bullet.tscn")
+var dir : String
+var moving : bool
+var CENTER : Vector2 = get_viewport_rect().size/2
+const SPEED : int = 200
+const ROTATION := {"U" : Vector2(0,-1), "D" : Vector2(0,1), "L" : Vector2(-1,0), "R" : Vector2(1,0),
+					"UR" : Vector2(0.5,-0.5), "UL" : Vector2(-0.5,-0.5), "DR" : Vector2(0.5,0.5), "DL" : Vector2(-0.5,0.5)}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	CENTER = get_viewport_rect().size/2
+	for i in ROTATION:
+		ROTATION[i] = ROTATION[i].normalized()
+	pass # Replace with function body.
+	
+func _process(delta):
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func _physics_process(delta):
+	moving = true
+	if Input.is_action_pressed("ui_up"):
+		if Input.is_action_pressed("ui_right"):
+			dir = "UR"
+		elif Input.is_action_pressed("ui_left"):
+			dir = "UL"
+		else:
+			dir = "U"
+	elif Input.is_action_pressed("ui_down"):
+		if Input.is_action_pressed("ui_right"):
+			dir = "DR"
+		elif Input.is_action_pressed("ui_left"):
+			dir = "DL"
+		else:
+			dir = "D"
+	elif Input.is_action_pressed("ui_right"):
+		dir = "R"
+	elif Input.is_action_pressed("ui_left"):
+		dir = "L"
+	else:
+		moving = false
+	if moving:
+		move(delta)
+
+func shoot():
+	var direction = (get_global_mouse_position() - position).normalized()
+	var bullet_ins = bullet.instance()
+	bullet_ins.ini(direction,position)
+	get_tree().get_root().add_child(bullet_ins)
+	print(dir)
+	
+func move(delta) -> void:
+	var movement = delta * SPEED * ROTATION[dir]
+	move_and_collide(movement)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
