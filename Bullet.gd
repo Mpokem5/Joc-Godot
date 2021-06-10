9a8ccf3beb5a4476bbf016a649a115f59ec475b6
Marc Garcia Clusella <marccc.garcia@gmail.com>
2021-06-11 00:41:34 +0200

    bala colisio parets

--- Bullet.gd
+++ Bullet.gd
@@ -21,3 +21,9 @@ func _physics_process(delta):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("wall"):
+		print("paret")
+		queue_free()
